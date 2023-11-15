04f09a671698cdb3e738026cda2d2f99cd145377
BrendanSmith <brendansmith464@gmail.com>
2023-11-15 17:08:30 +0800

    complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,33 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts 'Destorying all'
+Flat.destroy_all
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Pondok Ayu Guesthouse',
+  address: 'Brunch Club Berawa',
+  description: 'Room in a shared Guesthouse',
+  price_per_night: 25,
+  number_of_guests: 1
+)
+Flat.create!(
+  name: 'Big Flat in Canggu',
+  address: '42 Jalan Raya Samat, Canggu',
+  description: 'A big flat in Canggu with 1 bedroom and private bathroom',
+  price_per_night: 30,
+  number_of_guests: 1
+)
+Flat.create!(
+  name: 'Dark & Awful Garden Flat Birmingham',
+  address: '60 Broad Oaks Road B9 1DT',
+  description: 'An awful place',
+  price_per_night: 10,
+  number_of_guests: 5
+)
+
+puts 'Finished'
